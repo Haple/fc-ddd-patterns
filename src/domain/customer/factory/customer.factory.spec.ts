d086fee04f1c9a7fb3a8ceba4d5267a2f78cce06
santosalepholiveira@gmail.com <aleph.oliveira@picpay.com>
2022-09-25 06:50:43 -0300

    Customer events challenge completed

--- src/domain/customer/factory/customer.factory.spec.ts
+++ src/domain/customer/factory/customer.factory.spec.ts
@@ -1,22 +1,47 @@
 import CustomerFactory from "./customer.factory";
 import Address from "../value-object/address";
+import EventDispatcher from "../../@shared/event/event-dispatcher";
+import SendLog1WhenCustomerIsCreatedHandler from "../event/handler/send-log1-when-customer-is-created.handler";
+import SendLog2WhenCustomerIsCreatedHandler from "../event/handler/send-log2-when-customer-is-created.handler";
+import SendLogWhenCustomerAddressChangedHandler from "../event/handler/send-log-when-customer-address-changed.handler";
 
 describe("Customer factory unit test", () => {
   it("should create a customer", () => {
-    let customer = CustomerFactory.create("John");
+    const eventDispatcher = new EventDispatcher();
+    const eventHandler1 = new SendLog1WhenCustomerIsCreatedHandler();
+    const eventHandler2 = new SendLog2WhenCustomerIsCreatedHandler();
+    const spyEventHandler1 = jest.spyOn(eventHandler1, "handle");
+    const spyEventHandler2 = jest.spyOn(eventHandler2, "handle");
+    const spyEventDispatcherNotify = jest.spyOn(eventDispatcher, "notify");
+    eventDispatcher.register("CustomerCreatedEvent", eventHandler1);
+    eventDispatcher.register("CustomerCreatedEvent", eventHandler2);
+
+    const customerFactory = new CustomerFactory(eventDispatcher);
+    let customer = customerFactory.create("John");
 
     expect(customer.id).toBeDefined();
     expect(customer.name).toBe("John");
     expect(customer.Address).toBeUndefined();
+    expect(spyEventDispatcherNotify).toHaveBeenCalled();
+    expect(spyEventHandler1).toHaveBeenCalled();
+    expect(spyEventHandler2).toHaveBeenCalled();
   });
 
   it("should create a customer with an address", () => {
-    const address = new Address("Street", 1, "13330-250", "São Paulo");
+    const eventDispatcher = new EventDispatcher();
+    const eventHandler = new SendLogWhenCustomerAddressChangedHandler();
+    const spyEventHandler = jest.spyOn(eventHandler, "handle");
+    const spyEventDispatcherNotify = jest.spyOn(eventDispatcher, "notify");
+    eventDispatcher.register("CustomerAddressChangedEvent", eventHandler);
+    const customerFactory = new CustomerFactory(eventDispatcher);
 
-    let customer = CustomerFactory.createWithAddress("John", address);
+    const address = new Address("Street", 1, "13330-250", "São Paulo");
+    let customer = customerFactory.createWithAddress("John", address);
 
     expect(customer.id).toBeDefined();
     expect(customer.name).toBe("John");
     expect(customer.Address).toBe(address);
+    expect(spyEventDispatcherNotify).toHaveBeenCalled();
+    expect(spyEventHandler).toHaveBeenCalled();
   });
 });
--- src/domain/customer/factory/customer.factory.ts
+++ src/domain/customer/factory/customer.factory.ts
@@ -1,15 +1,28 @@
 import Customer from "../entity/customer";
 import { v4 as uuid } from "uuid";
 import Address from "../value-object/address";
+import EventDispatcher from "../../@shared/event/event-dispatcher";
+import CustomerCreatedEvent from "../event/customer-created.event";
+import CustomerAddressChangedEvent from "../event/customer-address-changed.event";
 
 export default class CustomerFactory {
-  public static create(name: string): Customer {
-    return new Customer(uuid(), name);
+
+  private _eventDispatcher: EventDispatcher;
+
+  constructor(eventDispatcher: EventDispatcher){
+    this._eventDispatcher = eventDispatcher;
+  }
+  
+  public create(name: string): Customer {
+    const customer = new Customer(uuid(), name);
+    this._eventDispatcher.notify(new CustomerCreatedEvent(customer))
+    return customer
   }
 
-  public static createWithAddress(name: string, address: Address): Customer {
+  public createWithAddress(name: string, address: Address): Customer {
     const customer = new Customer(uuid(), name);
     customer.changeAddress(address);
+    this._eventDispatcher.notify(new CustomerAddressChangedEvent(customer))
     return customer;
   }
 }
